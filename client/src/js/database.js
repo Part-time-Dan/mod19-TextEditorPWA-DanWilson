0af42f1e7a483a5be6855f71db0c0ce69a7683e4
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-04 11:31:08 -0600

    get/put logic, some webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,65 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+
+  try {
+
+    // Create a connection to the database database and version we want to use.
+    const jateDB = await openDB('jate', 1);
+
+    // Create a new transaction and specify the database and data privileges.
+    const tx = contactDb.transaction('jate', 'readwrite');
+
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+
+    // Use the .put() method to update an existing record.
+    const request = store.put(content);
+
+    // Commit the transaction to complete the update.
+    await tx.done;
+
+    const result = await request;
+    console.log('Data updated in the jate database.', result);
+
+  } catch (error) {
+    console.error('putDb not implemented', error)
+    throw error;
+  }
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  try {
+
+    // Create a connection to the database database and version we want to use.
+    const jateDB = await openDB('jate', 1);
+
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDB.transaction('jate', 'readonly');
+
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+
+    // Use the .getAll() method to get all data in the database.
+    const request = store.getAll();
+
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+
+
+  } catch (error) {
+    console.error('getDb not implemented', error)
+    throw error;
+  }
+
+};
 
 initdb();
